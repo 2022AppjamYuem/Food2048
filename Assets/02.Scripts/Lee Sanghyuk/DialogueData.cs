cc053065d5b84998f778ac7e37e7c922225e9818
MuYaHong2 <dltkdgur0409@gmail.com>
2022-12-17 19:25:03 +0900

    주문 수정

--- Assets/02.Scripts/Lee Sanghyuk/DialogueData.cs
+++ Assets/02.Scripts/Lee Sanghyuk/DialogueData.cs
@@ -23,13 +23,13 @@ namespace _02.Scripts.Lee_Sanghyuk
         }
         private readonly string[] _menuName = new []{
             "moneyflower",
-            "요세주면라비사와",
             "돈크츠 라멘 주세요 ···아니 역시 매운 라멘 주세요 그리고 와사비 추가해주시고요. ···아니다 그냥 뺴주세요",
             "곱빼기인듯곱빼기아닌곱빼기같지않지만 곱빼기로 주는척해주세요",
-            "고추냉이랑 와사비랑 다른거아시나요 그러니까 매운라면주세요",
             "치즈넣어주세요 없으면 와사비넣어주세요 아니다 와사비말고 그냥 주세요 매운라면주지마세요",
-            "많이줘요 2배로다가알아서",
+            "요세주면라비사와",
             "전 자연의 초록이 이쁘더라구요 그래서 와사비를 넣어주세요",
+            "고추냉이랑 와사비랑 다른거아시나요 그러니까 매운라면주세요",
+            "많이줘요 2배로다가알아서",
             "초록 묻고따블로 가~",
             "아따 배고프구마이 매운거 두개주소이"
         };
@@ -39,10 +39,31 @@ namespace _02.Scripts.Lee_Sanghyuk
             NPC.SetActive(true);
             NPC.transform.DOMove(new Vector3(1,1,1), 1).OnComplete(() =>
             {
-                var orderDetails = Random.Range(0, _menuName.Length);
-                var selectMenu = Random.Range(0, 5);
+                int orderDetails = 0;
+                var selectMenu = Random.Range(0, 6);
+                switch (selectMenu)
+                {
+                    case 0:
+                        orderDetails = Random.Range(0, 4);
+                        break;
+                    case 1:
+                        orderDetails = Random.Range(4, 6);
+                        break;
+                    case 2:
+                        orderDetails = 6;
+                        break;
+                    case 3:
+                        orderDetails = 7;
+                        break;
+                    case 4:
+                        orderDetails = 8;
+                        break;
+                    case 5:
+                        orderDetails = 9;
+                        break;
+                }
                 orderText.text = _menuName[orderDetails];
-                receip = (Food)(orderDetails);
+                receip = (Food)(selectMenu);
             });
         }
 
