1ed9629b1dfcf197f99cd9bfd5683bfa69a58854
정명직 <jmj51755245@gmail.com>
2022-12-18 04:38:02 +0900

    SoundManager add

---
+++ Assets/02.Scripts/SoundManager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource bgmAudioSource;
+    public AudioClip[] bgmList;
+
+    private static SoundManager instance = null;
+
+    void Awake()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+
+        if (null == instance)
+        {
+            //이 클래스 인스턴스가 탄생했을 때 전역변수 instance에 게임매니저 인스턴스가 담겨있지 않다면, 자신을 넣어준다.
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public static SoundManager Instance
+    {
+        get
+        {
+            if (null == instance)
+            {
+                return null;
+            }
+            return instance;
+        }
+    }
+
+    private void OnSceneLoaded(Scene arg0, LoadSceneMode arg1)
+    {
+
+        for (int i = 0; i < bgmList.Length; i++)
+        {
+            if (arg0.name == bgmList[i].name && bgmList[i] != null)
+            {
+                BgmPlay(bgmList[i]);
+            }
+        }
+    }
+
+    public void SFXPlay(string sfxName, AudioClip clip, float volume = 1f)
+    {
+        GameObject sfxGo = new GameObject(sfxName + "Sound");
+        AudioSource audioSource = sfxGo.AddComponent<AudioSource>();
+        audioSource.clip = clip;
+        audioSource.volume = volume;
+
+        audioSource.Play();
+        Destroy(sfxGo, clip.length);
+    }
+
+    public void BgmPlay(AudioClip clip)
+    {
+        bgmAudioSource.clip = clip;
+        bgmAudioSource.loop = true;
+        bgmAudioSource.volume = 0.1f;
+        bgmAudioSource.Play();
+    }
+}
